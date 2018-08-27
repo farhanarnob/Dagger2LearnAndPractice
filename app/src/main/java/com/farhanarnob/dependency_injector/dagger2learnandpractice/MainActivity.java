f821e93262fc5622c72cdbb81ea936f29569b43e
Farhan Rahman Arnob <arnob@field.buzz>
2018-08-27 12:14:22 +0600

    adding project

---
+++ app/src/main/java/com/farhanarnob/dependency_injector/dagger2learnandpractice/MainActivity.java
@@ -0,0 +1,15 @@
+package com.farhanarnob.dependency_injector.dagger2learnandpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvDate;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvDate = findViewById(R.id.tvDate);
+    }
+}
