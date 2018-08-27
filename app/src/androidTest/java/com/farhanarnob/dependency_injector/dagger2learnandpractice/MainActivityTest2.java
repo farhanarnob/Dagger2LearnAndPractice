159b0ae0354213f82143dda9fcf8981c13f58c82
Farhan Rahman Arnob <arnob@field.buzz>
2018-08-27 16:18:42 +0600

    Another espresso testing with Record Espresso Test

--- app/src/androidTest/java/com/farhanarnob/dependency_injector/dagger2learnandpractice/MainActivityTest2.java
+++ app/src/androidTest/java/com/farhanarnob/dependency_injector/dagger2learnandpractice/MainActivityTest2.java
@@ -1,6 +1,8 @@
 package com.farhanarnob.dependency_injector.dagger2learnandpractice;
 
+
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -13,18 +15,17 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withSubstring;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
-/**
- * Created by Farhan Rahman Arnob on 8/27/2018.
- */
-@RunWith(AndroidJUnit4.class)
 @LargeTest
-public class MainActivityTest {
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest2 {
 
     @Rule
-    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class){
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class){
         @Override
         protected void beforeActivityLaunched() {
             super.beforeActivityLaunched();
@@ -39,7 +40,11 @@ public class MainActivityTest {
     };
 
     @Test
-    public void formattedDateShown() {
-        onView(withId(R.id.tvDate)).check(matches(withSubstring("")));
+    public void mainActivityTest2() {
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.tvDate),
+                        isDisplayed()));
+        textView.check(matches(withText("Thu Jan 01 06:00:00 GMT+06:00 1970")));
+
     }
 }
